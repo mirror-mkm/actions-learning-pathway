fbfcd0832dc903d8a94fdcdd368c70a848bd9c6a
Mirror <73376645+mirror-mkm@users.noreply.github.com>
2025-01-30 15:20:31 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,12 +5,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  reactStrictMode: true,
 }
-
 module.exports = nextConfig
